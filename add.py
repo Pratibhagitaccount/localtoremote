c01dad19ccb41bc6f18e49914508cb0d8d315198
Pratibha <pratibha@Kunals-MacBook-Pro.local>
2022-04-21 13:40:24 -0400

    function3 added

--- add.py
+++ add.py
@@ -2,7 +2,10 @@ def fun1():
     print("i am the function 1")
 def fun2():
     print(" i am the function 2")
+def fun3():
+    print("hurray")
 
 if __name__ == "__main__":
     fun1()
     fun2()
+    fun3()
